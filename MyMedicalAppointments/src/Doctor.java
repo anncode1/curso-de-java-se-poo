14f3118a0813470db7c49475056d467e06831067
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-24 10:48:35 -0500

    Create a Class

---
+++ MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    //Atributos
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+
+}
---
+++ MyMedicalAppointments/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodríguez";
+        myDoctor.showName();
+
+    }
+}
