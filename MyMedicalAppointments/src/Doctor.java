84d9c10c1cb75938a9ef2e6ecd103eb6b3f9c0ba
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-24 11:03:08 -0500

    Metodo Constructor

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,8 +1,18 @@
+import javax.print.Doc;
+
 public class Doctor {
     //Atributos
     int id;
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
 
     //Comportamientos
     public void showName(){
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Anahí Salgado");
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
 
