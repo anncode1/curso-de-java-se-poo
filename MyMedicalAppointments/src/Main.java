352a3bb10d2b29135a6f12c46a6bd57ede85a120
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-25 08:36:53 -0500

    Final

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -3,16 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodríguez";
-        myDoctor.showName();
-        myDoctor.showId();
-
-
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-
         showMenu();
 
     }
--- MyMedicalAppointments/src/ui/UIMenu.java
+++ MyMedicalAppointments/src/ui/UIMenu.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class UIMenu {
 
+    public static String[] MONTHS = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -50,6 +52,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i +". " + MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
