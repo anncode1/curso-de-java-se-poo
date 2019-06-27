147f2cc226c21a237b7bc2040faf688815abd5f1
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-27 12:18:16 -0500

    Recorriendo estructuras de arbol en java

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class Doctor extends User {
     //Atributo
     private String speciality;
+    private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
 
     public Doctor(String name, String email){
         super(name,email);
@@ -22,7 +23,7 @@ public class Doctor extends User {
     }
 
 
-    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
     public void addAvailableAppointment(String date, String time){
         availableAppointments.add(new Doctor.AvailableAppointment(date,time));
     }
@@ -66,11 +67,11 @@ public class Doctor extends User {
             this.id = id;
         }
 
-        public Date getDate() {
+        public Date getDate(String DATE) {
             return date;
         }
 
-        public String getDate(String DATE) {
+        public String getDate() {
             return format.format(date);
         }
 
--- MyMedicalAppointments/src/ui/UIPatientMenu.java
+++ MyMedicalAppointments/src/ui/UIPatientMenu.java
@@ -1,6 +1,12 @@
 package ui;
 
+import model.Doctor;
+
+import java.rmi.server.UID;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class UIPatientMenu {
 
@@ -37,8 +43,33 @@ public class UIPatientMenu {
         do {
             System.out.println("::Book an appointment");
             System.out.println(":: Select date: ");
+            //Numeración de la lista de fechas
+            //Indice fecha seleccionada
+            //[doctors]
+            // 1.- doctor1
+                    // - 1 fecha1
+                    // - 2 fecha2
+            // 2.- doctor2
+            // 3.- doctor3
+            Map<Integer, Map<Integer, Doctor>> doctors = new TreeMap<>();
+            int k = 0;
+            for (int i = 0; i < UIDoctorMenu.doctorsAvailableAppointments.size(); i++) {
+                ArrayList<Doctor.AvailableAppointment> availableAppointments
+                        = UIDoctorMenu.doctorsAvailableAppointments.get(i).getAvailableAppointments();
 
-            
+                Map<Integer, Doctor>  doctorAppointments = new TreeMap<>();
+
+                for (int j = 0; j < availableAppointments.size(); j++) {
+                    k++;
+                    System.out.println(k + ". " + availableAppointments.get(j).getDate());
+                    doctorAppointments.put(Integer.valueOf(j), UIDoctorMenu.doctorsAvailableAppointments.get(i));
+
+                    doctors.put(Integer.valueOf(k), doctorAppointments);
+                }
+            }
+            Scanner sc = new Scanner(System.in);
+            int responseDateSelected = Integer.valueOf(sc.nextLine());
+
 
         }while (response!= 0);
     }
