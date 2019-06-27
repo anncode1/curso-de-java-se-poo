29c86970a6d60c309f321846e79ba904f9ca81fd
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-06-27 11:28:19 -0500

    Citas disponibles

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -23,7 +25,7 @@ public class Doctor extends User {
 
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
         availableAppointments.add(new Doctor.AvailableAppointment(date,time));
     }
 
@@ -47,9 +49,14 @@ public class Doctor extends User {
         private int id;
         private Date date;
         private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
-        public AvailableAppointment(Date date, String time) {
-            this.date = date;
+        public AvailableAppointment(String date, String time) {
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -64,6 +71,12 @@ public class Doctor extends User {
         public Date getDate() {
             return date;
         }
+
+        public String getDate(String DATE) {
+            return format.format(date);
+        }
+
+
 
         public void setDate(Date date) {
             this.date = date;
--- MyMedicalAppointments/src/ui/UIDoctorMenu.java
+++ MyMedicalAppointments/src/ui/UIDoctorMenu.java
@@ -1,8 +1,13 @@
 package ui;
 
+import model.Doctor;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UIDoctorMenu {
+
+    public static ArrayList<Doctor> doctorsAvailableAppointments = new ArrayList<>();
 
     public static void showDoctorMenu(){
         int response = 0;
@@ -58,7 +63,20 @@ public class UIDoctorMenu {
                 String date = sc.nextLine();
 
                 System.out.println("Your date is: " + date + "\n1. Correct \n2. Change Date");
-                
+                int responseDate = Integer.valueOf(sc.nextLine());
+                if (responseDate == 2) continue;
+
+                int responseTime = 0;
+                String time = "";
+                do {
+                    System.out.println("Insert the time available for date: " + date + " [16:00]");
+                    time = sc.nextLine();
+                    System.out.println("Your time is: " + time + "\n1. Correct \n2. Change Time");
+                    responseTime = Integer.valueOf(sc.nextLine());
+                }while (responseTime == 2);
+
+                UIMenu.doctorLogged.addAvailableAppointment(date,time);
+                checkDoctorAvailableAppointments(UIMenu.doctorLogged);
 
 
             }else if(response == 0){
@@ -66,5 +84,13 @@ public class UIDoctorMenu {
             }
 
         }while (response != 0);
+    }
+
+
+    private static void checkDoctorAvailableAppointments(Doctor doctor){
+        if (doctor.getAvailableAppointments().size() > 0
+            && !doctorsAvailableAppointments.contains(doctor)){
+            doctorsAvailableAppointments.add(doctor);
+        }
     }
 }
